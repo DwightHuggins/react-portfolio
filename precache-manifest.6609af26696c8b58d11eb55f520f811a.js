dba4a80cdcd87babc701f84ea5dbc90f27019368
Dwight Huggins <dwighthuggins@DwightHugginsJr.local>
2020-08-09 10:27:22 -0400

    Updates

---
+++ precache-manifest.6609af26696c8b58d11eb55f520f811a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5eb0931000dba8d04729ff2291feca6",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "de8e658be045f5f73a3f",
+    "url": "/react-portfolio/static/css/main.000a611b.chunk.css"
+  },
+  {
+    "revision": "1da725ce0e1dbba090fe",
+    "url": "/react-portfolio/static/js/2.72c2b9a0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.72c2b9a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de8e658be045f5f73a3f",
+    "url": "/react-portfolio/static/js/main.230f0471.chunk.js"
+  },
+  {
+    "revision": "cf38f6340ad39b6cf9da",
+    "url": "/react-portfolio/static/js/runtime-main.3eab8a52.js"
+  }
+]);
